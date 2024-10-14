5bfa7d793496b949e5786d8ebb69b2a5a1054a3a
Gaurav Singh <gaurav110498@gmail.com>
2024-10-14 13:21:42 +0530

    Built in and custom datastructures - Arrays, Maps, Stack

--- DSA/Datastructures/builtIn.js
+++ DSA/Datastructures/builtIn.js
@@ -1,34 +1,11 @@
-// const obj = {
-//     firstname : 'bruce',
-//     lastname : 'willis',
-//     empId : 191,
-//     location : 'Dallas',
-//     managerId : 34,
-//     salary : 45829.00
-// }
-
-// console.log(Object.keys(obj));
-// console.log(Object.values(obj));
-// console.log(Object.entries(obj));
-
-
-// const arr = [2, 34, 1, 5, 11, 0, 8]
-
-// const re = arr.splice(1,1)
-
-// const res = arr.map((num) => {return num * num})
-
-// const res2 = arr.filter(num => num % 2 === 0)
-
-// const res3 = arr.reduce((acc, num) => acc + num, 0)
-
-// console.log(arr, re, res, res2, res3);
-
-const array = () => {
+const arrays = () => {
     let s = [2, 34, 1, 5, 11, 0, 8, 'Biswas', 2.31, true]
     /**
      * Arrays in JS can store hetrogeneous data types
-     * 
+     * These are dynamically sized and are iterable data types
+     * shift/unshift/concat/slice/splice - O(n)
+     * push/pop - O(1)
+     * forEach/map/reduce - O(n)
      */
     s.push('hello')
     s.shift()
@@ -36,6 +13,75 @@ const array = () => {
     s.splice(2,1)
     for(let i of s)
         console.log(i);
+
+    const res = arr.map((num) => {return num * num})
+    const res2 = arr.filter(num => num % 2 === 0)
+    const res3 = arr.reduce((acc, num) => acc + num, 0)
+    console.log(arr, re, res, res2, res3);
 }
 
-array()
+const objects = () => {
+    /**
+     * An object is an unordered collection of key-value pairs.
+     * Objects are not iterable i.e. cannot use forOf loop
+     * Insert/Remove/Access - O(1)
+     * Search - O(n)
+     */
+    const obj = {
+        firstname : 'bruce',
+        lastname : 'willis',
+        empId : 191,
+        location : 'Dallas',
+        managerId : 34,
+        salary : 45829.00,
+        foo: function() {
+            return `${this.firstname} ${this.lastname}`
+        }
+    }
+    console.log(Object.keys(obj)); //O(n)
+    console.log(Object.values(obj)); //O(n)
+    console.log(Object.entries(obj)); //O(n)
+    console.log(obj.foo())
+
+}
+
+const sets = () => {
+    /**
+     * A set is an collection of unique values. Dynamic in size. 
+     * Order is not maintained. Sets are iterable
+     */
+    const s = new Set([1,2,3])
+    s.add(4)
+    for(let i of s)
+        console.log(i)
+    console.log(s.has(5), s.size, s.entries())
+    s.delete(4)
+    s.clear()
+}
+
+const maps = () => {
+    /**
+     * A map is an ordered collection of key-value pair
+     * Both keys and values can be of any data type
+     * Values are accessed with their corresponding key
+     * Unlike objects maps are iterable and can be used with a for of loop
+     * Unlike objects maps cannot store functions, it can only store data
+     */
+    const m = new Map([['a',1], ['b',2], ['c',3]])
+    m.set('d', 5)
+    for(let [k, v] of m)
+        console.log(k, v)
+
+    m.delete('d')
+    console.log(m.has('k'), m.size)
+    m.clear()
+}
+
+const sol = () => {
+    
+}
+
+
+(function main() {
+    sol();
+})();
---
+++ DSA/Datastructures/custom.js
@@ -0,0 +1,106 @@
+const stacks = () => {
+    /**
+     * Works on LIFO principle
+     * Both insertion and deletion from the same end(top)
+     * Example of use: Browser history tracking, Undo operation, Expression converstions, Call stack in JS runtime.
+     * Operations: push(), pop() & peek()
+     */
+
+    const s = new Stack([1,2,3,4,5]);
+    s.push(34)
+    console.log(s.pop())
+    console.log(s.peek())
+    s.size()
+}
+
+const queues = () => {
+    /**
+     * Works on FIFO principle
+     * insertion read end then deletion from the front end
+     * Example of use: Printers, CPU task scheduling, Callback queue in JS runtime.
+     * Operations: enqueue(), dequeue() & peek()
+     */
+
+    const q = new Queue();
+    q.enqueue(34)
+    q.enqueue(11)
+    console.log(q.isEmpty())
+    console.log(q.peek())
+    console.log(q.size())
+}
+
+class Stack {
+    constructor(initialItems = []) {
+        this.item = initialItems
+    }
+
+    push(e) {
+        this.item.push(e)
+    }
+
+    pop() {
+        if(!this.isEmpty())
+            return this.item.pop()
+        else
+            return `Stack is empty!`
+    }
+
+    peek() {
+        if(!this.isEmpty())
+            return this.item[this.item.length - 1];
+        else
+            return `Stack is empty!`
+    }
+
+    isEmpty() {
+        return this.item.length === 0
+    }
+
+    size() {
+        return this.item.length
+    }
+
+    clear() {
+        this.item = []
+    }
+}
+
+class Queue {
+    constructor(initialItems = []) {
+        this.items = initialItems
+        this.read = 0
+        this.front = 0
+    }
+
+    enqueue(e) {
+        this.items[this.rear] = e
+        this.rear++
+    }
+
+    dequeue() {
+        const i = this.items[this.front]
+        delete this.items[this.front]
+        this.front++
+        return i
+    }
+
+    isEmpty() {
+        return this.read - this.front === 0
+    }
+
+    peek() {
+        return this.items[this.front]
+    }
+
+    size() {
+        return this.rear - this.front
+    }
+
+    print() {
+        console.log(this.items)
+    }
+}
+
+(function mail() {
+    queues()
+})();
